2453a833f72033cc101181e5164d9195fbac791a
Ruben Pingol <128448242+rubenberttpingol@users.noreply.github.com>
2025-09-02 18:53:47 +0800

    Feature: XMR basic implementation (#2)
    * Add npm prepare script to pre-build lib based on source when using git commit hash
    
    * Add forceUpdateChromeOS command action

--- src/modules/xmr/xmr.ts
+++ src/modules/xmr/xmr.ts
@@ -1,21 +1,21 @@
 /*
  * Copyright (C) 2025 Xibo Signage Ltd
  *
- * Xibo - Digital Signage - https://www.xibosignage.com
+ * Xibo - Digital Signage - https://xibosignage.com
  *
  * This file is part of Xibo.
  *
  * Xibo is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
  * Xibo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
  */
 import {createNanoEvents, type Emitter} from 'nanoevents';
@@ -31,6 +31,7 @@ export interface XmrEvents {
     screenShot: () => void;
     licenceCheck: () => void;
     showStatusWindow: (timeout: number) => void;
+    forceUpdateChromeOS: () => void;
 }
 
 export default class Xmr {
@@ -192,6 +193,8 @@ export default class Xmr {
                 } else if (message.action == 'commandAction' && message.commandCode.startsWith('showStatusWindow')) {
                     const split = message.commandCode.split('|');
                     this.emitter.emit('showStatusWindow', parseInt(split[1]) || 60);
+                } else if (message.action == 'commandAction' && message.commandCode.startsWith('forceUpdateChromeOS')) {
+                    this.emitter.emit('forceUpdateChromeOS');
                 } else {
                     console.error('Xmr::message: unknown action: ' + message.action);
                 }
